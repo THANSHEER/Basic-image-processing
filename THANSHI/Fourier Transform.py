931170e147fc88446a78b3ecfbd1f802a9acb89c
MOHAMMED THANSEER <111374295+THANSHEER@users.noreply.github.com>
2023-05-23 10:29:48 +0530

    Delete THANSHI directory

--- THANSHI/Fourier Transform.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import cv2
-# Reading the Image
-img = cv2.imread('example.jpeg',cv2.IMREAD_UNCHANGED)
-domainFilter = cv2.edgePreservingFilter(img, flags=1, sigma_s=60, sigma_r=0.6)
-cv2.imshow('Domain Filter',np.hstack((img,domainFilter)))
-cv2.waitKey(0)
-cv2.destroyAllWindows()
